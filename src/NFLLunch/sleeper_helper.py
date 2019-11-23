58d11100849f5a89e95a8b7e1079da69fd35a662
Zach Hugnou <zachary.hugnou@gmail.com>
2019-11-23 14:00:44 -0500

    Changes made: * move ff_espn_helper to the correct directory * sleeper_helper: Add logging. Fix methods and how variables are assigned

--- src/NFLLunch/sleeper_helper.py
+++ src/NFLLunch/sleeper_helper.py
@@ -1,8 +1,10 @@
+import logging
 from datetime import datetime
 from typing import Union
 import sleeper_wrapper as sleeper
 
 YEAR = datetime.now().year
+LOG = logging.getLogger("NFLLunch.sleeper_helper")
 # helper = SleeperHelper(sleeper, 11)
 
 
@@ -11,16 +13,31 @@ class SleeperHelper:
         self, sleeper, week, season=YEAR, season_type='regular'
     ) -> None:
         self.client = sleeper
-        self.season = season
-        self.season_type = season_type
-        self.week = week
-        self.all_players = self.client.Players().get_all_players()
-        self.defenses = {
-            d: v for d, v in self.all_players.items() if v['position'] == 'DEF'
+
+    def __get_all_players(self):
+        """
+            Return all NFL players and defenses
+        """
+        return self.client.Players().get_all_players()
+
+    def __get_all_deffensive_players(self):
+        """
+            Return all defensive NFL players
+        """
+        return {
+            d: v
+            for d, v in self.__get_all_players.items()
+            if v['position'] == 'DEF'
         }
-        self.players = {
-            k: self.all_players[k]
-            for k in set(self.all_players) - set(self.defenses)
+
+    def __get_all_offensive_players(self):
+        """
+            Return all offensive NFL players
+        """
+        all_players = self.__get_all_players
+        return {
+            k: all_players[k]
+            for k in set(all_players) - set(self.__get_all_deffensive_players)
         }
 
     def id_from_player_name(self, full_name: str) -> Union[str, None]:
@@ -28,11 +45,14 @@ class SleeperHelper:
             Return the ID of a given player
         """
         full_name = full_name.title()
-        for player_id, v in self.players.items():
+        for player_id, v in self.__get_all_offensive_players.items():
             try:
                 if v['full_name'] == full_name:
                     return player_id
             except KeyError:
+                logging.info(
+                    "KeyError: No 'full_name' for player %s", player_id
+                )
                 continue
         return None
 
@@ -41,14 +61,11 @@ class SleeperHelper:
             Return the ID of a given team's defense.
         """
         def_name = def_name.lower()
-        for def_id, v in self.defenses.items():
-            try:
-                del v['fantasy_positions']
-                del v['active']
-                if def_name in [t.lower() for t in list(v.values())]:
-                    return def_id
-            except KeyError:
-                continue
+        for def_id, v in self.__get_all_deffensive_players.items():
+            del v['fantasy_positions']
+            del v['active']
+            if def_name in [t.lower() for t in list(v.values())]:
+                return def_id
         return None
 
 
